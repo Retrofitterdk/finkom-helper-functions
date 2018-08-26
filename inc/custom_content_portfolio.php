7c91f8b62c3b1395c4f85d39133e4ae4be82f520
sbangnielsen <sbn@dagensmedicin.dk>
2018-08-26 09:04:23 +0200

    Customize Custom Content Portfolio

--- inc/custom_content_portfolio.php
+++ inc/custom_content_portfolio.php
@@ -1,19 +1,19 @@
 <?php
 
 /**
- * Change name of Custom Content Portfolio post type.
- */
+* Change name of Custom Content Portfolio post type.
+*/
 function finkom_ccp_portfolio_posttype($portfolio_posttype) {
-	$portfolio_posttype = _x( 'project', 'portfolio posttype', 'finkom_helper_functions' );
+  $portfolio_posttype = _x( 'project', 'portfolio posttype', 'finkom_helper_functions' );
 
-	return $portfolio_posttype;
+  return $portfolio_posttype;
 }
 
 add_filter('ccp_get_project_post_type', 'finkom_ccp_portfolio_posttype');
 
 /**
- * Add REST API support to the Custom Content Portfolio post type.
- */
+* Add REST API support to the Custom Content Portfolio post type.
+*/
 add_action( 'init', 'finkom_ccp_rest_support', 25 );
 function finkom_ccp_rest_support() {
   global $wp_post_types;
@@ -26,19 +26,19 @@ function finkom_ccp_rest_support() {
 }
 
 /**
- * Change name of Custom Content Portfolio category.
- */
+* Change name of Custom Content Portfolio category.
+*/
 function finkom_ccp_category_name($category_name) {
-	$category_name = _x( 'project_category', 'category name', 'finkom_helper_functions' );
+  $category_name = _x( 'project_category', 'category name', 'finkom_helper_functions' );
 
-	return $category_name;
+  return $category_name;
 }
 
 add_filter('ccp_get_category_taxonomy', 'finkom_ccp_category_name');
 
 /**
- * Add REST API support to the Custom Content Portfolio category.
- */
+* Add REST API support to the Custom Content Portfolio category.
+*/
 add_action( 'init', 'finkom_ccp_category_rest_support', 25 );
 function finkom_ccp_category_rest_support() {
   global $wp_taxonomies;
@@ -53,19 +53,19 @@ function finkom_ccp_category_rest_support() {
 }
 
 /**
- * Change name of Custom Content Portfolio tags.
- */
+* Change name of Custom Content Portfolio tags.
+*/
 function finkom_ccp_tag_name($tag_name) {
-	$tag_name = _x( 'project_tag', 'tag name', 'finkom_helper_functions' );
+  $tag_name = _x( 'project_tag', 'tag name', 'finkom_helper_functions' );
 
-	return $tag_name;
+  return $tag_name;
 }
 
 add_filter('ccp_get_tag_taxonomy', 'finkom_ccp_tag_name');
 
 /**
- * Add REST API support to the Custom Content Portfolio category.
- */
+* Add REST API support to the Custom Content Portfolio category.
+*/
 add_action( 'init', 'finkom_ccp_tag_rest_support', 25 );
 function finkom_ccp_tag_rest_support() {
   global $wp_taxonomies;
@@ -76,6 +76,28 @@ function finkom_ccp_tag_rest_support() {
   if ( isset( $wp_taxonomies[ $taxonomy_name ] ) ) {
     $wp_taxonomies[ $taxonomy_name ]->show_in_rest = true;
 
+  }
+}
+
+function finkom_project_meta( $before = '', $after = '', $echo = true ) {
+  if ( in_the_loop() && ccp_is_single_project() && ccp_is_project() && ! post_password_required() ) {
+
+    $project_meta = '';
+
+    $project_meta .= $before;
+
+    $project_meta .= ccp_get_project_link(       array( 'text' => esc_html__( 'Visit Project', 'finkom_helper_functions' ), 'before' => '<li>', 'after' => '</li>' ) );
+    $project_meta .= ccp_get_project_client(     array( 'text' => esc_html__( 'Client: %s',    'finkom_helper_functions' ), 'before' => '<li>', 'after' => '</li>' ) );
+    $project_meta .= ccp_get_project_location(   array( 'text' => esc_html__( 'Location: %s',  'finkom_helper_functions' ), 'before' => '<li>', 'after' => '</li>' ) );
+    $project_meta .= ccp_get_project_start_date( array( 'text' => esc_html__( 'Started: %s',   'finkom_helper_functions' ), 'before' => '<li>', 'after' => '</li>' ) );
+    $project_meta .= ccp_get_project_end_date(   array( 'text' => esc_html__( 'Completed: %s', 'finkom_helper_functions' ), 'before' => '<li>', 'after' => '</li>' ) );
+
+    $project_meta .= $after;
+
+    if ( $echo )
+        echo $project_meta;
+    else
+        return $project_meta;
   }
 }
 
